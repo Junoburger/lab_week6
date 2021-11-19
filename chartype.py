1674c9924cc3b460ee3180e1c8689f9f3103137a
Junoburger <juno.burger@pm.me>
2021-11-19 09:17:05 +0100

    Add lower case check

---
+++ chartype.py
@@ -0,0 +1,14 @@
+char = input("Enter a character: ")
+
+
+if (char.isnumeric() is False and char.isalpha() is False):
+    print(char, "is a non-alphanumeric character.")
+
+if (char.isnumeric() and char.isalpha() is False):
+    print(char, "is  a digit.")
+
+if (char == char.upper() and char.isalpha() is True):
+    print(char, "is an upper case letter.")
+
+if (char == char.lower() and char.isalpha() is True):
+    print(char, "is an lower case letter.")
